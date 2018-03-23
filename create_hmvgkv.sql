52660116493f94fdfb67ea8e70ffa47565c72038
Benjamin Moeller <37707273+LupusE@users.noreply.github.com>
2018-03-23 17:59:10 +0100

    First public version

---
+++ create_hmvgkv.sql
@@ -0,0 +1,32 @@
+USE hmvdb;
+
+-- format:
+-- <hv:GRUPPE>13</hv:GRUPPE>
+--        <hv:ORT>20</hv:ORT>
+--        <hv:UNTERGRUPPE>3</hv:UNTERGRUPPE>
+--        <hv:ART>5</hv:ART>
+--        <hv:PRODUKT>151</hv:PRODUKT>
+
+-- HMV: <hv:GRUPPE><hv:ORT><hv:UNTERGRUPPE><hv:ART><hv:PRODUKT>
+
+DROP TABLE IF EXISTS hmvgkv;
+
+CREATE TABLE hmvgkv ( 
+	hv_GRUPPE TINYINT, -- 13
+	hv_ORT TINYINT, -- 20
+	hv_UNTERGRUPPE TINYINT, -- 01,02,03,08,12,14,...
+	hv_ART TINYINT, -- 0-9
+	hv_PRODUKT SMALLINT, -- 000-999 (lfd)
+	hv_BEZEICHNUNG text, -- Model
+	hv_HERSTELLER text, -- Vendor
+	hv_MERKMALE text, -- Description
+	hv_AUFNAHMEDATUM date,
+	hv_AENDERUNGSDATUM date,
+	created TIMESTAMP
+	);
+    
+LOAD XML LOCAL INFILE 'C:\\git\\hmv\\20180302_HMV\\20180302_HMV_2.xml'
+	INTO TABLE hmvdb.hmvgkv
+	CHARACTER SET UTF8
+	ROWS IDENTIFIED BY '<hv_HMV_PRODUKT>'
+	;
